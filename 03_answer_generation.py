f910037955841fd5abe6ad717dcf24a64b6a83d0
Asifmahmud436 <safaandsafa4@gmail.com>
2025-12-31 13:07:48 +0600

    bot has memory now

--- 03_answer_generation.py
+++ 03_answer_generation.py
@@ -19,9 +19,6 @@ query = "How much did Microsoft pay to acquire GitHub"
 retriever = db.as_retriever(search_kwargs={"k":5})
 
 relevant_docs = retriever.invoke(query)
-# print(f"User Query: {query}")
-# for i, doc in enumerate(relevant_docs, 1):
-#     print(f"Document {i}:\n{doc.page_content}\n")
 
 combined_input = f"""Based on the follwing documents, please answer this question: {query}
 Documents:
